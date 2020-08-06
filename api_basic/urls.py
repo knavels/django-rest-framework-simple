c1edce1c5b1f9099bf957d4e13e85938b8c9f9b6
Navid Dezashibi <dezashibi@gmail.com>
2020-08-06 13:33:12 +0430

    added security to generic api 👍😎

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -4,6 +4,7 @@ from .views import article_list, article, ArticleListAPIView, ArticleShowAPIView
 urlpatterns = [
     # path('articles/', article_list),
     path('articles/', ArticleListAPIView.as_view()),
+    path('gen-articles/', ArticleGenericAPIView.as_view()),
     path('gen-articles/<int:id>', ArticleGenericAPIView.as_view()),
     # path('articles/<int:pk>', article),
     path('articles/<int:id>', ArticleShowAPIView.as_view()),
--- api_basic/views.py
+++ api_basic/views.py
@@ -13,6 +13,10 @@ from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework import mixins
 
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
 
 #######
 # Generic Views
@@ -23,12 +27,16 @@ class ArticleGenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixi
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
     lookup_field = 'id'
+    # authentication_classes = [SessionAuthentication, BasicAuthentication]
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
 
     def get(self, request, id=None):
         if id:
             return self.retrieve(request)
-
-        return self.list(request)
+        else:
+            return self.list(request)
 
     def post(self, request):
         return self.create(request)
